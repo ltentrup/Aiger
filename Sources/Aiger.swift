d1c00d51e66ec1996fb79253401bec818104b173
Leander Tentrup <tentrup@react.uni-saarland.de>
2016-09-19 15:05:45 +0200

    Implement deallocation and literal lookup

--- Sources/Aiger.swift
+++ Sources/Aiger.swift
@@ -1,19 +1,37 @@
 import CAiger
 
-public struct Aiger {
+public class Aiger {
     let _aiger: UnsafeMutablePointer<aiger>
+    internal var _resetOnDealloc: Bool
     
-    public init() {
-        _aiger = aiger_init()
+    /// fallback: provide access to C aiger struct
+    public var aiger: UnsafeMutablePointer<aiger> {
+        return _aiger
     }
     
-    public init(from: String) {
-        self.init()
+    public init(resetOnDealloc: Bool = true) {
+        _aiger = aiger_init()
+        _resetOnDealloc = resetOnDealloc
+    }
+    
+    public convenience init?(from: String, resetOnDealloc: Bool = true) {
+        self.init(resetOnDealloc: resetOnDealloc)
         aiger_read_from_string(_aiger, from)
     }
     
-    public init(from: UnsafeMutablePointer<aiger>) {
+    public init(from: UnsafeMutablePointer<aiger>, resetOnDealloc: Bool = false) {
         _aiger = from
+        _resetOnDealloc = resetOnDealloc
+    }
+    
+    deinit {
+        if _resetOnDealloc {
+            aiger_reset(_aiger)
+        }
+    }
+    
+    public var maxVar: UInt32 {
+        return _aiger.pointee.maxvar
     }
     
     public var inputs: CArrayOverlay<aiger_symbol> {
@@ -31,6 +49,34 @@ public struct Aiger {
     public var ands: CArrayOverlay<aiger_and> {
         return CArrayOverlay(address: _aiger.pointee.ands, length: Int(_aiger.pointee.num_ands))
     }
+    
+    public func tag(forLit lit: UInt32) -> AigerTag {
+        precondition(lit % 2 == 0)
+        switch aiger_lit2tag(_aiger, lit) {
+        case 0:
+            // Constant
+            return .Constant
+        case 1:
+            // Input
+            return .Input(aiger_is_input(_aiger, lit)!.pointee)
+        case 2:
+            // Latch
+            return .Latch(aiger_is_latch(_aiger, lit)!.pointee)
+        case 3:
+            // And
+            return .And(aiger_is_and(_aiger, lit)!.pointee)
+        default:
+            assert(false)
+            abort()
+        }
+    }
+}
+
+public enum AigerTag {
+    case Constant
+    case Input(aiger_symbol)
+    case Latch(aiger_symbol)
+    case And(aiger_and)
 }
 
 public struct CArrayOverlay<T>: Sequence, IteratorProtocol {
--- Tests/AigerTests/AigerTests.swift
+++ Tests/AigerTests/AigerTests.swift
@@ -3,19 +3,22 @@ import XCTest
 
 class AigerTests: XCTestCase {
     func testIterator() {
-        let aiger = Aiger(from: "aag 3 2 0 1 1\n2\n4\n6\n6 2 4\n")
-        assert(aiger.inputs.underestimatedCount == 2)
+        guard let aiger = Aiger(from: "aag 3 2 0 1 1\n2\n4\n6\n6 2 4\n") else {
+            XCTFail()
+            return
+        }
+        XCTAssert(aiger.inputs.underestimatedCount == 2)
         for symbol in aiger.inputs {
-            assert(symbol.lit == 2 || symbol.lit == 4)
+            XCTAssert(symbol.lit == 2 || symbol.lit == 4)
         }
-        assert(aiger.outputs.underestimatedCount == 1)
+        XCTAssert(aiger.outputs.underestimatedCount == 1)
         for symbol in aiger.outputs {
-            assert(symbol.lit == 6)
+            XCTAssert(symbol.lit == 6)
         }
-        assert(aiger.latches.underestimatedCount == 0)
-        assert(aiger.ands.underestimatedCount == 1)
+        XCTAssert(aiger.latches.underestimatedCount == 0)
+        XCTAssert(aiger.ands.underestimatedCount == 1)
         for and in aiger.ands {
-            assert(and.lhs == 6)
+            XCTAssert(and.lhs == 6)
         }
     }
 

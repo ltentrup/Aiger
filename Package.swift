a322e2c2557415c7526b2f1c1ac6d757d71536bd
Leander Tentrup <tentrup@react.uni-saarland.de>
2019-04-09 11:02:09 +0200

    Update PckageDescription to v4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,16 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Aiger",
+    products: [
+        .library(name: "Aiger", targets: ["Aiger"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/ltentrup/CAiger.git", majorVersion: 0, minor: 1)
+        .package(url: "https://github.com/ltentrup/CAiger.git", from: "0.1.0"),
+    ],
+    targets: [
+        .target(name: "Aiger", dependencies: ["CAiger"]),
+        .testTarget(name: "AigerTests", dependencies: ["Aiger"]),
     ]
 )
